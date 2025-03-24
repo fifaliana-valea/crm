0fe2067e6a4c4305726bd87dda31c56bb79a09ee
Fifaliana_valea <valeafifaliana@gmail.com>
2025-03-24 20:24:31 +0300

    login rest api

--- src/main/java/site/easy/to/build/crm/config/SecurityConfig.java
+++ src/main/java/site/easy/to/build/crm/config/SecurityConfig.java
@@ -2,18 +2,12 @@ package site.easy.to.build.crm.config;
 
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.core.env.Environment;
 import org.springframework.security.authentication.AuthenticationManager;
-<<<<<<< Updated upstream
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-=======
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
->>>>>>> Stashed changes
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -29,76 +23,55 @@ import site.easy.to.build.crm.config.api.ApiUserDetails;
 import site.easy.to.build.crm.config.api.JWTFilter;
 import site.easy.to.build.crm.config.oauth2.CustomOAuth2UserService;
 import site.easy.to.build.crm.config.oauth2.OAuthLoginSuccessHandler;
-import site.easy.to.build.crm.service.user.OAuthUserService;
-import site.easy.to.build.crm.util.StringUtils;
 
-import java.util.Optional;
 
 @Configuration
 public class SecurityConfig {
 
-        private final OAuthLoginSuccessHandler oAuth2LoginSuccessHandler;
+    private final OAuthLoginSuccessHandler oAuth2LoginSuccessHandler;
 
-        private final CustomOAuth2UserService oauthUserService;
+    private final CustomOAuth2UserService oauthUserService;
 
-        private final CrmUserDetails crmUserDetails;
+    private final CrmUserDetails crmUserDetails;
 
-        private final CustomerUserDetails customerUserDetails;
+    private final CustomerUserDetails customerUserDetails;
 
-        private final Environment environment;
+    private final Environment environment;
 
-        private final JWTFilter jwtFilter;
+    private final JWTFilter jwtFilter;
 
-<<<<<<< Updated upstream
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
-    }
+    private final ApiAuthenticationEntryPoint apiAuthenticationEntryPoint;
+
+    private final ApiUserDetails apiUserDetails;
+
     @Autowired
-    public SecurityConfig(OAuthLoginSuccessHandler oAuth2LoginSuccessHandler, CustomOAuth2UserService oauthUserService, CrmUserDetails crmUserDetails,
-                          CustomerUserDetails customerUserDetails, Environment environment) {
+    public SecurityConfig(OAuthLoginSuccessHandler oAuth2LoginSuccessHandler,
+                          CustomOAuth2UserService oauthUserService, CrmUserDetails crmUserDetails,
+                          CustomerUserDetails customerUserDetails, Environment environment, JWTFilter jwtFilter,
+                          ApiAuthenticationEntryPoint apiAuthenticationEntryPoint,
+                          ApiUserDetails apiUserDetails) {
         this.oAuth2LoginSuccessHandler = oAuth2LoginSuccessHandler;
         this.oauthUserService = oauthUserService;
         this.crmUserDetails = crmUserDetails;
         this.customerUserDetails = customerUserDetails;
         this.environment = environment;
+        this.jwtFilter = jwtFilter;
+        this.apiAuthenticationEntryPoint = apiAuthenticationEntryPoint;
+        this.apiUserDetails = apiUserDetails;
     }
-=======
-        private final ApiAuthenticationEntryPoint apiAuthenticationEntryPoint;
->>>>>>> Stashed changes
 
-        private final ApiUserDetails apiUserDetails;
+    @Bean
+    @Order(3)
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        @Autowired
-        public SecurityConfig(OAuthLoginSuccessHandler oAuth2LoginSuccessHandler,
-                        CustomOAuth2UserService oauthUserService, CrmUserDetails crmUserDetails,
-                        CustomerUserDetails customerUserDetails, Environment environment, JWTFilter jwtFilter,
-                        ApiAuthenticationEntryPoint apiAuthenticationEntryPoint,
-                        ApiUserDetails apiUserDetails) {
-                this.oAuth2LoginSuccessHandler = oAuth2LoginSuccessHandler;
-                this.oauthUserService = oauthUserService;
-                this.crmUserDetails = crmUserDetails;
-                this.customerUserDetails = customerUserDetails;
-                this.environment = environment;
-                this.jwtFilter = jwtFilter;
-                this.apiAuthenticationEntryPoint = apiAuthenticationEntryPoint;
-                this.apiUserDetails = apiUserDetails;
-        }
+        HttpSessionCsrfTokenRepository httpSessionCsrfTokenRepository = new HttpSessionCsrfTokenRepository();
+        httpSessionCsrfTokenRepository.setParameterName("csrf");
 
-<<<<<<< Updated upstream
         http.csrf((csrf) -> csrf
-                .csrfTokenRepository(httpSessionCsrfTokenRepository)
-        );
-=======
-        @Bean
-        @Order(3)
-        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
->>>>>>> Stashed changes
+                .csrfTokenRepository(httpSessionCsrfTokenRepository));
 
-                HttpSessionCsrfTokenRepository httpSessionCsrfTokenRepository = new HttpSessionCsrfTokenRepository();
-                httpSessionCsrfTokenRepository.setParameterName("csrf");
+        http.authorizeHttpRequests((authorize) -> authorize
 
-<<<<<<< Updated upstream
                         .requestMatchers("/register/**").permitAll()
                         .requestMatchers("/set-employee-password/**").permitAll()
                         .requestMatchers("/change-password/**").permitAll()
@@ -111,133 +84,112 @@ public class SecurityConfig {
                         .requestMatchers(AntPathRequestMatcher.antMatcher("/**/manager/**")).hasRole("MANAGER")
                         .requestMatchers("/employee/**").hasAnyRole("MANAGER", "EMPLOYEE")
                         .requestMatchers("/customer/**").hasRole("CUSTOMER")
-                        .anyRequest().authenticated()
-                )
-=======
-                http.csrf((csrf) -> csrf
-                                .csrfTokenRepository(httpSessionCsrfTokenRepository));
->>>>>>> Stashed changes
+                        .anyRequest().authenticated())
 
-                http.authorizeHttpRequests((authorize) -> authorize
+                .formLogin((form) -> form
+                        .loginPage("/login")
+                        .loginProcessingUrl("/login")
+                        .defaultSuccessUrl("/", true)
+                        .failureUrl("/login")
+                        .permitAll())
+                .userDetailsService(crmUserDetails)
+                .oauth2Login(oauth2 -> oauth2
+                        .loginPage("/login")
+                        .userInfoEndpoint(userInfo -> userInfo
+                                .userService(oauthUserService))
+                        .successHandler(oAuth2LoginSuccessHandler))
+                .logout((logout) -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/login")
+                        .permitAll())
+                .exceptionHandling(exception -> {
+                    exception.accessDeniedHandler(accessDeniedHandler());
+                });
 
-                                .requestMatchers("/register/**").permitAll()
-                                .requestMatchers("/set-employee-password/**").permitAll()
-                                .requestMatchers("/change-password/**").permitAll()
-                                .requestMatchers("/font-awesome/**").permitAll()
-                                .requestMatchers("/fonts/**").permitAll()
-                                .requestMatchers("/images/**").permitAll()
-                                .requestMatchers("/save").permitAll()
-                                .requestMatchers("/js/**").permitAll()
-                                .requestMatchers("/css/**").permitAll()
-                                .requestMatchers(AntPathRequestMatcher.antMatcher("/**/manager/**")).hasRole("MANAGER")
-                                .requestMatchers("/employee/**").hasAnyRole("MANAGER", "EMPLOYEE")
-                                .requestMatchers("/customer/**").hasRole("CUSTOMER")
-                                .anyRequest().authenticated())
+        return http.build();
+    }
 
-                                .formLogin((form) -> form
-                                                .loginPage("/login")
-                                                .loginProcessingUrl("/login")
-                                                .defaultSuccessUrl("/", true)
-                                                .failureUrl("/login")
-                                                .permitAll())
-                                .userDetailsService(crmUserDetails)
-                                .oauth2Login(oauth2 -> oauth2
-                                                .loginPage("/login")
-                                                .userInfoEndpoint(userInfo -> userInfo
-                                                                .userService(oauthUserService))
-                                                .successHandler(oAuth2LoginSuccessHandler))
-                                .logout((logout) -> logout
-                                                .logoutUrl("/logout")
-                                                .logoutSuccessUrl("/login")
-                                                .permitAll())
-                                .exceptionHandling(exception -> {
-                                        exception.accessDeniedHandler(accessDeniedHandler());
-                                });
+    @Bean
+    public AccessDeniedHandler accessDeniedHandler() {
+        return new CustomAccessDeniedHandler();
+    }
 
-                return http.build();
-        }
+    @Bean
+    @Order(2)
+    public SecurityFilterChain customerSecurityFilterChain(HttpSecurity http) throws Exception {
 
-        @Bean
-        public AccessDeniedHandler accessDeniedHandler() {
-                return new CustomAccessDeniedHandler();
-        }
+        HttpSessionCsrfTokenRepository httpSessionCsrfTokenRepository = new HttpSessionCsrfTokenRepository();
+        httpSessionCsrfTokenRepository.setParameterName("csrf");
 
-        @Bean
-        @Order(2)
-        public SecurityFilterChain customerSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf((csrf) -> csrf
+                .csrfTokenRepository(httpSessionCsrfTokenRepository));
 
-                HttpSessionCsrfTokenRepository httpSessionCsrfTokenRepository = new HttpSessionCsrfTokenRepository();
-                httpSessionCsrfTokenRepository.setParameterName("csrf");
+        http.securityMatcher("/customer-login/**").authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers("/set-password/**").permitAll()
+                        .requestMatchers("/font-awesome/**").permitAll()
+                        .requestMatchers("/fonts/**").permitAll()
+                        .requestMatchers("/images/**").permitAll()
+                        .requestMatchers("/js/**").permitAll()
+                        .requestMatchers("/css/**").permitAll()
+                        .requestMatchers(AntPathRequestMatcher.antMatcher("/**/manager/**")).hasRole("MANAGER")
+                        .anyRequest().authenticated())
+                .formLogin((form) -> form
+                        .loginPage("/customer-login")
+                        .loginProcessingUrl("/customer-login")
+                        .failureUrl("/customer-login")
+                        .defaultSuccessUrl("/", true)
+                        .permitAll())
+                .userDetailsService(customerUserDetails)
+                .logout((logout) -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/customer-login")
+                        .permitAll());
 
-                http.csrf((csrf) -> csrf
-                                .csrfTokenRepository(httpSessionCsrfTokenRepository));
+        return http.build();
+    }
 
-                http.securityMatcher("/customer-login/**").authorizeHttpRequests((authorize) -> authorize
-                                .requestMatchers("/set-password/**").permitAll()
-                                .requestMatchers("/font-awesome/**").permitAll()
-                                .requestMatchers("/fonts/**").permitAll()
-                                .requestMatchers("/images/**").permitAll()
-                                .requestMatchers("/js/**").permitAll()
-                                .requestMatchers("/css/**").permitAll()
-                                .requestMatchers(AntPathRequestMatcher.antMatcher("/**/manager/**")).hasRole("MANAGER")
-                                .anyRequest().authenticated())
-                                .formLogin((form) -> form
-                                                .loginPage("/customer-login")
-                                                .loginProcessingUrl("/customer-login")
-                                                .failureUrl("/customer-login")
-                                                .defaultSuccessUrl("/", true)
-                                                .permitAll())
-                                .userDetailsService(customerUserDetails)
-                                .logout((logout) -> logout
-                                                .logoutUrl("/logout")
-                                                .logoutSuccessUrl("/customer-login")
-                                                .permitAll());
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
-                return http.build();
-        }
+    @Bean
+    @Order(1)
+    public SecurityFilterChain apiSecurityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .securityMatcher("/api/**") // Isole les routes API
+                .csrf(csrf -> csrf.disable()) // Désactive CSRF pour les appels API (JSON)
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/login").permitAll()
+                        .anyRequest().authenticated())
+                .formLogin(AbstractHttpConfigurer::disable) // Désactive le formulaire HTML
+                .httpBasic(AbstractHttpConfigurer::disable) // Désactive l’auth basic
+                .exceptionHandling(exception -> exception
+                        .authenticationEntryPoint((request, response, authException) -> {
+                            response.setContentType("application/json");
+                            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                            response.getWriter().write("{\"error\": \"Unauthorized\"}");
+                        })
+                        .accessDeniedHandler((request, response, accessDeniedException) -> {
+                            response.setContentType("application/json");
+                            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+                            response.getWriter().write("{\"error\": \"Access Denied\"}");
+                        }))
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+        ;
 
-        @Bean
-        public PasswordEncoder passwordEncoder() {
-                return new BCryptPasswordEncoder();
-        }
+        return http.build();
+    }
 
-        @Bean
-        @Order(1)
-        public SecurityFilterChain apiSecurityFilterChain(HttpSecurity http) throws Exception {
-                http
-                                .securityMatcher("/api/**") // Isole les routes API
-                                .csrf(csrf -> csrf.disable()) // Désactive CSRF pour les appels API (JSON)
-                                .sessionManagement(session -> session
-                                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                                .authorizeHttpRequests(auth -> auth
-                                                .requestMatchers("/api/login").permitAll()
-                                                .anyRequest().authenticated())
-                                .formLogin(AbstractHttpConfigurer::disable) // Désactive le formulaire HTML
-                                .httpBasic(AbstractHttpConfigurer::disable) // Désactive l’auth basic
-                                .exceptionHandling(exception -> exception
-                                                .authenticationEntryPoint((request, response, authException) -> {
-                                                        response.setContentType("application/json");
-                                                        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-                                                        response.getWriter().write("{\"error\": \"Unauthorized\"}");
-                                                })
-                                                .accessDeniedHandler((request, response, accessDeniedException) -> {
-                                                        response.setContentType("application/json");
-                                                        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
-                                                        response.getWriter().write("{\"error\": \"Access Denied\"}");
-                                                }))
-                                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
-                ;
-
-                return http.build();
-        }
-
-        @Bean
-        public AuthenticationManager authenticationManager(HttpSecurity http, PasswordEncoder passwordEncoder)
-                        throws Exception {
-                return http.getSharedObject(AuthenticationManagerBuilder.class)
-                                .userDetailsService(apiUserDetails)
-                                .passwordEncoder(passwordEncoder)
-                                .and()
-                                .build();
-        }
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http, PasswordEncoder passwordEncoder)
+            throws Exception {
+        return http.getSharedObject(AuthenticationManagerBuilder.class)
+                .userDetailsService(apiUserDetails)
+                .passwordEncoder(passwordEncoder)
+                .and()
+                .build();
+    }
 }
