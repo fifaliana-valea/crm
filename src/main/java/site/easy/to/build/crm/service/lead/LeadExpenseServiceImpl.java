4615691e73fb2bb694aa124834f40ef6df373960
Fifaliana_valea <valeafifaliana@gmail.com>
2025-03-25 10:32:12 +0300

    modifier et affichage list

--- src/main/java/site/easy/to/build/crm/service/lead/LeadExpenseServiceImpl.java
+++ src/main/java/site/easy/to/build/crm/service/lead/LeadExpenseServiceImpl.java
@@ -1,7 +1,12 @@
 package site.easy.to.build.crm.service.lead;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
+import site.easy.to.build.crm.entity.Lead;
 import site.easy.to.build.crm.entity.LeadExpense;
 import site.easy.to.build.crm.repository.LeadExpenseRepository;
 @Service
@@ -20,5 +25,16 @@ public class LeadExpenseServiceImpl implements LeadExpenseService{
     @Override
     public LeadExpense findLatestByTriggerLeadHistoId(Integer triggerLeadHistoId) {
         return leadExpenseRepository.findLatestByTriggerLeadHistoId(triggerLeadHistoId);
+    }
+
+    @Override
+    public LeadExpense findById(int id) {
+        Optional<LeadExpense> result = leadExpenseRepository.findById(id);
+
+        if (result.isEmpty()) {
+            throw new RuntimeException("Dépense non trouvée avec l'ID: " + id);
+        }
+
+        return result.get();
     }
 }
